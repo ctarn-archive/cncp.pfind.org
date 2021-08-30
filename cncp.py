cc5b607a5cfc3304e32149df5b9916f07c51dde5
Tarn Yeong Ching <i@ctarn.io>
2021-08-30 18:26:17 +0800

    cncp: add 会议日程

--- cncp.py
+++ cncp.py
@@ -35,6 +35,20 @@ def speaker(dir_out, tmpl):
         file.write(tmpl.render(items=items))
 
 
+def schedule(dir_out, tmpl):
+    items = {}
+    with open(os.path.join('data', 'speaker2021.csv')) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            items[row['id']] = row
+    for k, v in items.items():
+        with open(os.path.join('data', 'talk', '2021', f'{k}.html')) as file:
+            v['talk'] = file.read()
+    os.makedirs(os.path.join(dir_out, 'schedule'), exist_ok=True)
+    with open(os.path.join(dir_out, 'schedule', 'index.html'), 'w') as file:
+        file.write(tmpl.render(items=items))
+
+
 def main():
     parser = argparse.ArgumentParser(description='CNCP Website Generator')
     parser.add_argument('dst', type=str, metavar='dst')
@@ -47,6 +61,7 @@ def main():
     env = Environment(loader=loader)
     index(dir_out, env.get_template('index.html'))
     speaker(dir_out, env.get_template('speaker.html'))
+    schedule(dir_out, env.get_template('schedule.html'))
     for page in ['about', 'register', 'analysis', 'cxms/register']:
         os.makedirs(os.path.join(dir_out, page), exist_ok=True)
         with open(os.path.join(dir_out, page, 'index.html'), 'w') as file:
